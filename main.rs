8fe70a111f5adaa165534ac878ce1494e987ae98
PytheasLeMassaliote <165064277+PytheasLeMassaliote@users.noreply.github.com>
2024-04-01 16:24:34 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -14,19 +14,19 @@ struct Work {
     author: Option<String>,
     title: Option<String>,
     english: Option<String>,
-    /*original: Option<String>,
+    original: Option<String>,
     domains: Option<String>,
-    r#type: Option<String>,
+    rtype: Option<String>,
     translator: Option<String>,
     translated_from: Option<String>,
     date: Option<String>,
     publication_type: Option<String>,
     author_editor: Option<String>,
     title_b: Option<String>,
-    r#in: Option<String>,
+    rin: Option<String>,
     published: Option<String>,
-    volumes: Option<String>,
-    pages: Option<String>,*/
+    volume: Option<String>,
+    pages: Option<String>,
 }
 
 async fn test() {
@@ -60,51 +60,235 @@ async fn test() {
                             .next()
                             .and_then(|span| Some(span.text().collect::<String>())).as_deref()
                             .map(str::to_owned);
+
                         let title = html_product
                             .select(&Selector::parse(".work_title").unwrap())
                             .next()
                             .and_then(|span| Some(span.text().collect::<String>())).as_deref()
                             .map(str::to_owned);
-                        let english = html_product
-                            .select(&Selector::parse("p").unwrap())
-                            .next()
-                            .and_then(|strong| strong.next_sibling().text().collect::<String>()).as_deref()
-                            .map(str::to_owned);
 
-                            
+                        let english = html_product
+                            .select(&Selector::parse("strong").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("English:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let original = html_product
+                            .select(&Selector::parse("strong").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Original:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let domains = html_product
+                            .select(&Selector::parse("strong").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Original:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let rtype = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Type:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let translator = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Translator:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let translated_from = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Translated from:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let date = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Date:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let publication_type = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Publication type:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let author_editor = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| strong.text().any(|text| text.contains("Author/Editor:")))
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                    .next_sibling()
+                                    .and_then(|node| node.value().as_text())
+                                    .map(|text| text.trim().to_owned())
+                            })
+                            .unwrap_or_else(|| "Pas d'information trouvé".to_owned());
+
+                            let title_b = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| {
+                                strong.text().any(|text| text.contains("Title:"))
+                            })
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                .next_sibling()
+                                .and_then(|node| node.value().as_text())
+                                .map(|text| text.trim().to_owned())
+                            });
+
+                            let rin = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| strong.text().any(|text| text.contains("in:")))
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                    .next_sibling()
+                                    .and_then(|node| node.value().as_text())
+                                    .map(|text| text.trim().to_owned())
+                            })
+                            .unwrap_or_else(|| "Pas d'information trouvé".to_owned());
+
+                            let published = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| strong.text().any(|text| text.contains("Published:")))
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                    .next_sibling()
+                                    .and_then(|node| node.value().as_text())
+                                    .map(|text| text.trim().to_owned())
+                            })
+                            .unwrap_or_else(|| "Pas d'information trouvé".to_owned());
+
+                            let volume = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| strong.text().any(|text| text.contains("Volume:")))
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                    .next_sibling()
+                                    .and_then(|node| node.value().as_text())
+                                    .map(|text| text.trim().to_owned())
+                            })
+                            .unwrap_or_else(|| "Pas d'information trouvé".to_owned());
+
+                            let pages = html_product
+                            .select(&Selector::parse("b").unwrap())
+                            .filter(|strong| strong.text().any(|text| text.contains("Pages:")))
+                            .next()
+                            .and_then(|strong| {
+                                strong
+                                    .next_sibling()
+                                    .and_then(|node| node.value().as_text())
+                                    .map(|text| text.trim().to_owned())
+                            })
+                            .unwrap_or_else(|| "Pas d'information trouvé".to_owned());
 
 
                         let works = Work {
                             author,
                             title,
                             english,
-                            /*original,
+                            original,
                             domains,
-                            r#type,
+                            rtype,
                             translator,
                             translated_from,
                             date,
                             publication_type,
-                            author_editor,
-                            title,
-                            r#in,
-                            published,
-                            volumes,
-                            pages,*/
+                            author_editor: Some(author_editor),
+                            title_b,
+                            rin: Some(rin),
+                            published: Some(published),
+                            volume: Some(volume),
+                            pages: Some(pages),
                         };
                         work.push(works);
 
                         let mut csv_writer = Writer::from_path("products.csv").unwrap();
-                        csv_writer.write_record(&["Author", "Title", "English"/*, "Original", "Domains", "Type", "Translator", "Translated from",
-                         "Date", "Publication type", "Author/Editor", "Title", "In", "Published", "Volumes", "Pages"*/]).unwrap();
+                        csv_writer.write_record(&["Author", "Title", "English", "Original", "Domains", "Type", "Translator", "Translated from",
+                         "Date", "Publication type", "Author/Editor", "Title", "In", "Published", "Volume", "Pages"]).unwrap();
                         
 
                         for product in &work {
                             let author = product.author.clone().unwrap();
                             let title = product.title.clone().unwrap();
                             let english = product.english.clone().unwrap();
-                            csv_writer.write_record(&[author, title, english]).unwrap();
-                            
+                            let original = product.original.clone().unwrap();
+                            let domains = product.domains.clone().unwrap();
+                            let rtype = product.rtype.clone().unwrap();
+                            let translator = product.translator.clone().unwrap();
+                            let translated_from = product.translated_from.clone().unwrap();
+                            let date = product.date.clone().unwrap();
+                            let publication_type = product.publication_type.clone().unwrap();
+                            let author_editor = product.author_editor.clone().unwrap();
+                            let title_b = product.title.clone().unwrap();
+                            let rin = product.rin.clone().unwrap();
+                            let published = product.published.clone().unwrap();
+                            let volume = product.volume.clone().unwrap();
+                            let pages = product.pages.clone().unwrap();
+                            csv_writer.write_record(&[author, title, english, original, domains, rtype, translator, translated_from,
+                            date, publication_type, author_editor, title_b, rin, published, volume, pages]).unwrap();
                             csv_writer.flush().unwrap();
                         }
                     }
@@ -112,4 +296,5 @@ async fn test() {
             }
         }
     }
+}
 }
